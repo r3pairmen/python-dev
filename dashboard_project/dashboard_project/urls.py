25b185f25e0c9e1e00ee1f1c86214a4b38770a43
Vintage <89855073+r3pairmen@users.noreply.github.com>
2023-07-20 06:43:31 +0530

    Delete dashboard_project directory

--- dashboard_project/dashboard_project/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from dashboard_app.views import get_filtered_data
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/data/', get_filtered_data, name='get_filtered_data'),
-]
